e883a839c1680b38c13410fe2fd8019176f85d87
sravan410 <g.sravankumarreddy410@gmail.com>
2021-06-07 19:09:00 +0530

    first commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post, Author, Tag 
+from .models import Post, Author, Tag, Comment
 
 # Register your models here.
 
@@ -11,7 +11,12 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug":("title",)}
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
